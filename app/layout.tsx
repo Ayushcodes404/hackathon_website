66bffc92becb3c302549f51698a9056939d95dcf
AyushT <81374784+Ayushcodes404@users.noreply.github.com>
2025-08-05 17:07:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: ['H-Fest 2025]',
+  title: ['H-Fest 2025'],
 }
 
 export default function RootLayout({
