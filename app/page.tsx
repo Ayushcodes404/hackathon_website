957291ca1c9bf19e733aa48056080d473ffb73c8
Ayushcodes404 <ayushkyt@gmail.coom>
2025-08-05 12:19:35 +0530

    Final Deployment

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function HackathonWebsite() {
               Turn your wildest ideas into reality.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center items-center mb-12">
-              <Link href="https://forms.gle/N9wPdDkYvD7FaPBq9">
+              <Link href="/https://forms.gle/N9wPdDkYvD7FaPBq9">
                 <button className="sketchy-btn bg-[#FF6B35] text-white px-8 py-4 text-xl font-semibold">
                     Register Now
                   </button>
@@ -868,7 +868,7 @@ export default function HackathonWebsite() {
             collaboration.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
-            <Link href="/https://forms.gle/N9wPdDkYvD7FaPBq9">
+            <Link href="https://forms.gle/N9wPdDkYvD7FaPBq9">
             <button className="sketchy-btn bg-white text-[#FF6B35] px-8 py-4 text-xl font-semibold">
               Register Now 
             </button>
